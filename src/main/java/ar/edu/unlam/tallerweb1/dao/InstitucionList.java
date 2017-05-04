1ad338a4d30d8167cc87771560208049423d8b09
f-alvarez <floor.alvarez@gmail.com>
2017-05-03 21:54:56 -0300

    Creacion Proyecto Base

---
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/InstitucionList.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.tallerweb1.dao;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.Institucion;
+
+public class InstitucionList {
+
+   Institucion intitu = new Institucion();
+	
+   List<Institucion> lista = new LinkedList<Institucion>();   
+	
+  //
+}
